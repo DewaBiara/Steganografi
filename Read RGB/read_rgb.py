f0e8cbde7a791718efae8dd91e54b0d2488d34a9
DewaBiara <angabiara@gmail.com>
2023-10-13 08:52:38 +0800

    Tugas read RGB

--- Read RGB/read_rgb.py
+++ Read RGB/read_rgb.py
@@ -1,16 +1,20 @@
 from PIL import Image
 
 # Buka gambar BMP
-image_path = "2.bmp"
-img = Image.open(image_path)
+image = "2.bmp"
+img = Image.open(image)
 
 # Mendapatkan dimensi gambar
 width, height = img.size
 
-# Membaca nilai RGB dari gambar
-for y in range(height):
-    for x in range(width):
-        pixel = img.getpixel((x, y))
-        red, green, blue = pixel[0], pixel[1], pixel[2]
+# Meminta pengguna untuk memasukkan titik (x, y)
+x = int(input("Masukkan nilai x: "))
+y = int(input("Masukkan nilai y: "))
 
-        print(f"Pixel di posisi ({x}, {y}): R={red}, G={green}, B={blue}")
+# Pastikan titik (x, y) berada dalam batas gambar
+if 0 <= x < width and 0 <= y < height:
+    pixel = img.getpixel((x, y))
+    red, green, blue = pixel[0], pixel[1], pixel[2]
+    print(f"Pixel di posisi ({x}, {y}): R={red}, G={green}, B={blue}")
+else:
+    print("Titik (x, y) berada di luar batas gambar.")
